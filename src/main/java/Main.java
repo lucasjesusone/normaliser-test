088e08fd6dc568b9886f0dac707f5521b8bf15a9
lucasjesusone <lucasjesusone@gmail.com>
2022-02-26 14:46:11 -0400

    Implementing class that returns the best match for the provides input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,30 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
 
-        String jt = "Java engineer";
+
         Normaliser n = new Normaliser();
-        String normalisedTitle = n.normalise(jt);
-        //output normalisedTitle
-        jt = "C# engineer";
-        normalisedTitle = n.normalise(jt);
-        //output normalisedTitle
-        jt = "Chief Accountant";
-        normalisedTitle = n.normalise(jt);
-        //output normalisedTitle
+
+        String c = "C# engineer";
+        String normalisedTitle = n.normalise(c);
+        Double qualityScore = n.compareStrings(normalisedTitle, c);
+        System.out.printf("\nInput: %s\nOutput: %s\nSimilarity: %.2f\n", c, normalisedTitle, qualityScore);
+
+        String jt = "Java engineer";
+        String normalisedJavaEngineerTitle = n.normalise(jt);
+        double qualityJavaEngineerScore = n.compareStrings(normalisedJavaEngineerTitle, jt);
+        System.out.printf("\nInput: %s\nOutput: %s\nSimilarity: %.2f\n", jt, normalisedJavaEngineerTitle, qualityJavaEngineerScore);
+
+
+
+        String a = "Accountant";
+        String normalisedAccountantTitle = n.normalise(a);
+        double qualityAccountantScore = n.compareStrings(normalisedAccountantTitle, a);
+        System.out.printf("\nInput: %s\nOutput: %s\nSimilarity: %.2f\n", a, normalisedAccountantTitle, qualityAccountantScore);
+
+
+        String ca = "Chief Accountant";
+        String normalisedChiefAccountantTitle = n.normalise(ca);
+        double qualityChiefAccountantScore = n.compareStrings(normalisedChiefAccountantTitle, ca);
+        System.out.printf("\nInput: %s\nOutput: %s\nSimilarity: %.2f\n", ca, normalisedChiefAccountantTitle, qualityChiefAccountantScore);
     }
 }
--- src/main/java/Normaliser.java
+++ src/main/java/Normaliser.java
@@ -1,8 +1,66 @@
+import org.jboss.jandex.IndexReader;
+import org.jboss.jandex.IndexWriter;
+import org.springframework.expression.ParseException;
+
+import javax.swing.text.Document;
+import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Normaliser {
+    public String normalise(String input) {
+        String[] n = {"Architect", "Software engineer", "Quantity surveyor", "Accountant"};
+        String normalisedTitle = null;
+        double q = 0;
 
+        for (String s : n) {
+            double result = compareStrings(input, s);
 
-    public String normalise(String jt) {
-        System.out.println(jt);
-        return jt;
-    };
+            if (result > q || q == 0) {
+                q = result;
+                normalisedTitle = s;
+            }
+        }
+        return normalisedTitle;
+    }
+
+    public double compareStrings(String str1, String str2) {
+        List<String> pairs1 = wordLetterPairs(str1.toUpperCase());
+        List<String> pairs2 = wordLetterPairs(str2.toUpperCase());
+        int intersection = 0;
+        int union = pairs1.size() + pairs2.size();
+        for (Object pair1 : pairs1) {
+            for (int j = 0; j < pairs2.size(); j++) {
+                Object pair2 = pairs2.get(j);
+                if (pair1.equals(pair2)) {
+                    intersection++;
+                    pairs2.remove(j);
+                    break;
+                }
+            }
+        }
+        return (2.0 * intersection) / union;
+    }
+
+    private List<String> wordLetterPairs(String str) {
+        List<String> allPairs = new ArrayList<String>();
+        String[] words = str.split("\\s");
+        for (String word : words) {
+            String[] pairsInWord = letterPairs(word);
+            Collections.addAll(allPairs, pairsInWord);
+        }
+        return allPairs;
+    }
+
+    private String[] letterPairs(String str) {
+        int numPairs = str.length() - 1;
+        String[] pairs = new String[numPairs];
+        for (int i = 0; i < numPairs; i++) {
+            pairs[i] = str.substring(i, i + 2);
+        }
+        return pairs;
+    }
 }
